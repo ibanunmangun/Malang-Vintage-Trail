d5ee51aa9f6da0643d21fdf5272f6b125053f8de
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-05-23 17:47:23 +0700

    Delete data directory

--- data/Meancoordinates_3.js
+++
@@ -1,8 +0,0 @@
-var json_Meancoordinates_3 = {
-"type": "FeatureCollection",
-"name": "Meancoordinates_3",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "MEAN_X": 112.63037650941989, "MEAN_Y": -7.9818039534967324 }, "geometry": { "type": "Point", "coordinates": [ 112.630376509419889, -7.981803953496732 ] } }
-]
-}
