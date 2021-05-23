30eaa88ae40b3cb741364ab8868d490f444a685d
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-05-23 17:52:38 +0700

    Add files via upload

---
+++ data/Meancoordinates_4.js
@@ -0,0 +1,8 @@
+var json_Meancoordinates_4 = {
+"type": "FeatureCollection",
+"name": "Meancoordinates_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
---
+++ data/TrailMalang2_1.js
@@ -0,0 +1,13 @@
+var json_TrailMalang2_1 = {
+"type": "FeatureCollection",
+"name": "TrailMalang2_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Trail Malang", "descriptio": "Jarak Total 2800 Meter" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 112.627624166681798, -7.980894147251623 ], [ 112.62802, -7.981 ], [ 112.62838, -7.9811 ], [ 112.62853, -7.98114 ], [ 112.62893, -7.98123 ], [ 112.62897, -7.98124 ], [ 112.62904, -7.98126 ], [ 112.62917, -7.98129 ], [ 112.62944, -7.98136 ], [ 112.62953, -7.98139 ], [ 112.6299, -7.9815 ], [ 112.62994, -7.98151 ], [ 112.63017, -7.98157 ], [ 112.63033, -7.98161 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "Trail Malang", "descriptio": "Jarak Total 2800 Meter" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 112.63033, -7.98161 ], [ 112.63044, -7.98102 ], [ 112.63046, -7.98086 ], [ 112.63046, -7.98071 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "Trail Malang", "descriptio": "Jarak Total 2800 Meter" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 112.63033, -7.98161 ], [ 112.63041, -7.98163 ], [ 112.63059, -7.98166 ], [ 112.63085, -7.98175 ], [ 112.6309, -7.98176 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "Trail Malang", "descriptio": "Jarak Total 2800 Meter" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 112.63135, -7.98361 ], [ 112.63238563294712, -7.983904492350594 ], [ 112.633956570466239, -7.984414943964418 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "Trail Malang", "descriptio": "Jarak Total 2800 Meter" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 112.633956570466239, -7.984414943964418 ], [ 112.634096759116218, -7.983970463480006 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "Trail Malang", "descriptio": "Jarak Total 2800 Meter" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 112.6309, -7.98176 ], [ 112.63104, -7.9818 ], [ 112.63139, -7.9819 ], [ 112.63154, -7.98202 ], [ 112.63166, -7.98212 ], [ 112.63154, -7.98266 ], [ 112.63135, -7.98361 ] ], [ [ 112.633956570466239, -7.984414943964418 ], [ 112.634239421683574, -7.984504004989125 ] ] ] } }
+]
+}
